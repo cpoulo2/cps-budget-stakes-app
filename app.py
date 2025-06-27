e0bebcc028f738062a882ce3d33912cf94efdd2e
cpoulo2 <cdpoulos@gmail.com>
2025-06-27 14:40:45 -0500

    fixing html print scale

--- app.py
+++ app.py
@@ -1096,7 +1096,7 @@ def main():
                                     width: 7.5in !important;
                                     max-width: 7.5in !important;
                                     font-size: 10px; 
-                                    zoom: .85;
+                                    zoom: .8;
                                     table-layout: fixed;
                                     page-break-inside: auto;
                                 }}
